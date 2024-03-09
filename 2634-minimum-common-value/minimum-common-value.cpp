7b0e80a0035422ad75041c144437e539b0b1131d
Jitendra-rec <65058396+Jitendra-rec@users.noreply.github.com>
2024-03-09 10:21:47 +0530

    Time: 64 ms (82.93%) | Memory: 53 MB (51.54%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -5,7 +5,6 @@ public:
         int i=0,j=0,l1=n1.size(),l2=n2.size();
         while(i<l1 && j<l2 && n1[i]!=n2[j])
         {
-             cout<<n1[i]<<". "<<n2[j]<<endl;
             if(n1[i]==n2[j])
             return n1[i];
             else if(n1[i]>n2[j])
