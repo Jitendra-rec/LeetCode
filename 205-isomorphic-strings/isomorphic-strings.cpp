3dd30f2da4e6c84fe5fef3bf86b08314e49c22ac
Jitendra-rec <65058396+Jitendra-rec@users.noreply.github.com>
2024-04-02 19:47:26 +0530

    Time: 7 ms (48.84%) | Memory: 9.3 MB (42.66%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -2,21 +2,34 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) 
     {
-        map<char,int> mp1,mp2;
-        for(char ch:s)
-        {
-            if( mp1[ch] > 0) continue;
-            mp1[ch]=mp1.size();
-        }
-        for(char ch:t)
-        {
-            if( mp2[ch] > 0) continue;
-            mp2[ch]=mp2.size();
-        }
+        unordered_map<char,char> mp1;
+        unordered_map<char,bool> mp2;
         for(int i=0;i<s.length();i++)
         {
-            if(mp1[s[i]] != mp2[t[i]]) return 0;
+            if(mp2.find(t[i])==mp2.end())
+            {
+                mp2[t[i]]=true;
+                 if(mp1.find(s[i])==mp1.end())
+                    mp1[s[i]]=t[i];
+                    else 
+                    return 0;
+            }
+            else
+            {
+                //if(mp1.find(s[i])==mp1.end())
+                if(mp1[s[i]]!=t[i])
+                return false;
+
+            }
+
         }
+        // for(auto i:mp1)
+        // cout<<i.first<< " ---- "<<i.second<<endl;
+
+        //         for(auto i:mp2)
+        // cout<<i.first<< " ---- "<<i.second<<endl;
+
+        
         return 1;
     }
 };
