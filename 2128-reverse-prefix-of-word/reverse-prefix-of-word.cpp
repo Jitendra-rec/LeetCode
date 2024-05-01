345b6bb6f8948d053722841ecd01763beab4f7c1
Jitendra-rec <65058396+Jitendra-rec@users.noreply.github.com>
2024-05-01 12:21:13 +0530

    Time: 3 ms (42.21%) | Memory: 7.6 MB (26.16%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -3,7 +3,6 @@ public:
     string reversePrefix(string word, char ch) {
         int n=word.find(ch);
         string str="";
-        cout<<n<<endl;
         if(n!=-1)
         reverse(word.begin(),word.begin()+n+1);
         cout<<word;
