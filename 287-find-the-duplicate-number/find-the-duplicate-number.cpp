3b72c90e4860a6636c00ae43d4d043d4fab75c00
Jitendra-rec <65058396+Jitendra-rec@users.noreply.github.com>
2024-06-23 23:51:43 +0530

    Time: 70 ms (85.26%) | Memory: 63.7 MB (53.86%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -14,19 +14,19 @@ public:
     //    return 0;
     // 
             // Approch 2
-    for(int i=0;i <nums.size();i++)
-    {
-        if(nums[abs(nums[i])]<0)
-        return abs(nums[i]);
-        else
-        {
-            nums[abs(nums[i])]*=-1;
-        }
-    }
-        return 0;
+    // for(int i=0;i <nums.size();i++)
+    // {
+    //     if(nums[abs(nums[i])]<0)
+    //     return abs(nums[i]);
+    //     else
+    //     {
+    //         nums[abs(nums[i])]*=-1;
+    //     }
+    // }
+    //     return 0;
 
         // approch 3
-    while(nums[nums[0]]==nums[0]){
+    while(nums[nums[0]]!=nums[0]){
         swap(nums[0],nums[nums[0]]);
     }
     return nums[0];
