94f606b1b2e31de2dd0a67fe0868d3ec406d2a78
Jitendra-rec <65058396+Jitendra-rec@users.noreply.github.com>
2024-07-04 12:16:46 +0530

    Time: 79 ms (68.72%) | Memory: 62 MB (44.79%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -1,15 +1,37 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int n=arr.size(),max_index=0,max=INT_MIN;
-     for(int i=0;i<n;i++)
-     {
-        if(arr[i]>max)
-        {
-            max=arr[i];
-            max_index=i;
-        }
-     }
-     return max_index;
+        // Travel and find max
+
+    // int n=arr.size(),max_index=0,max=INT_MIN;
+    //  for(int i=0;i<n;i++)
+    //  {
+    //     if(arr[i]>max)
+    //     {
+    //         max=arr[i];
+    //         max_index=i;
+    //     }
+    //  }
+    //  return max_index;
+    // 
+// finding using inbuilt function but in this it return only number of maximum
+
+    // auto ans=max_element(arr.begin(),arr.end());
+    // return *ans;
+    
+    int start=0,end=arr.size()-1;
+    
+    while(start<end)
+    {
+        int mid=start + (end-start)/2;
+
+        if(arr[mid]< arr[mid+1])
+        start=mid+1;
+        else
+        end=mid;
     }
+        return start;
+    }
+
+
 };
