c8ed083f3b510fd4e943612c6a3fe6b9b5d293dc
Jitendra-rec <65058396+Jitendra-rec@users.noreply.github.com>
2024-07-07 14:41:20 +0530

    Time: 2 ms (53.84%) | Memory: 7.2 MB (30.43%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -3,7 +3,7 @@ public:
     int numWaterBottles(int numBottles, int numExchange) {
         int ans=numBottles,emptyBottle=0;
         while(numBottles>=numExchange)
-        {   cout<<ans<<" "<<numBottles<<endl;
+        {   //cout<<ans<<" "<<numBottles<<endl;
             ans+=numBottles/numExchange;
             numBottles=numBottles/numExchange+numBottles%numExchange;
         }
