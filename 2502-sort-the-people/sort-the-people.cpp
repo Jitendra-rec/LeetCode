0a8d901e231a362f1dc9b51169b896e320e3e59b
Jitendra-rec <65058396+Jitendra-rec@users.noreply.github.com>
2024-07-22 18:02:16 +0530

    Time: 39 ms (26.12%) | Memory: 29.1 MB (21.29%) - LeetSync

--- 2502-sort-the-people/sort-the-people.cpp
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -3,18 +3,32 @@ public:
 
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
        
-        for(int i=0;i<heights.size();i++)
+        unordered_map<int ,string>mp;
+        for(int i=0;i<names.size();i++)
         {
-            for(int j=0;j<heights.size();j++)
-            {
-                if(heights[i]>heights[j])
-                {
-                    swap(heights[i],heights[j]);
-                    swap(names[i],names[j]);
-                }
-            }
-            
+            mp[heights[i]]=names[i];
         }
-        return names;
+
+        sort(heights.rbegin(),heights.rend());
+       
+        for(int i=0;i<names.size();i++)
+        {
+               names[i] = mp[heights[i]];  
+        }
+         return names;
+    //     for(int i=0;i<heights.size();i++)
+    //     {
+    //         for(int j=0;j<heights.size();j++)
+    //         {
+    //             if(heights[i]>heights[j])
+    //             {
+    //                 swap(heights[i],heights[j]);
+    //                 swap(names[i],names[j]);
+    //             }
+    //         }
+            
+    //     }
+    //     return names;
+    // }
     }
 };
