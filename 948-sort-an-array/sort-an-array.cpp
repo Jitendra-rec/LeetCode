089a623c8fa0f2b1b6f5c3fbf9ecc238e271c9b4
Jitendra-rec <65058396+Jitendra-rec@users.noreply.github.com>
2024-07-25 17:28:05 +0530

    Time: 92 ms (83.25%) | Memory: 70.4 MB (68.95%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,19 +1,7 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-    //     int n=nums.size();
-    //     for(int i=0;i<n-1;i++)
-    //     {     int min=i;
-    //         for(int j=i+1;j<n;j++)
-    //         {
-    //             if(nums[min]>nums[j])
-    //             min=j;
-    //         }
-    //         swap(nums[i],nums[min]);
-    //     }
-    //     return nums;  
-    // 
-    sort(nums.begin(),nums.end());
-    return nums;
+        sort(nums.begin(),nums.end());
+        return nums;
     }
 };
