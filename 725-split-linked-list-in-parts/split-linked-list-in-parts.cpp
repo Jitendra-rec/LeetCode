8c4ef1c8defae636813fb11e39841f60f81e7aa2
Jitendra-rec <65058396+Jitendra-rec@users.noreply.github.com>
2024-08-01 18:46:34 +0530

    Time: 22 ms (16.21%) | Memory: 13.6 MB (33.25%) - LeetSync

--- 725-split-linked-list-in-parts/split-linked-list-in-parts.cpp
+++ 725-split-linked-list-in-parts/split-linked-list-in-parts.cpp
@@ -10,6 +10,15 @@
  */
 class Solution {
 public:
+    void p(ListNode* head)
+    {
+        while(head)
+        {
+            cout<<head->val<<"->";
+            head=head->next;
+        }
+        cout<<"NULL"<<endl;
+    }
     vector<ListNode*> splitListToParts(ListNode* head, int k) {
         vector<ListNode*>arr(k,NULL);
         int len=0;
@@ -19,17 +28,28 @@ public:
             len++;
             temp=temp->next;
         }
+   
         temp=head;
         int l=len/k;
         int m=len%k;
+        int dm=l;
 
         for(int i=0; i<k && temp;i++)
-        {
-           arr[i]=temp; 
-           int re=l+ (m-->0?1:0);
-           for(int j=1;j<re;j++)
-           temp=temp->next;
-      
+        { dm=l;
+           arr[i]=temp;
+           //cout<<l<<" "<<m<<endl;
+           if(m>0)
+           {
+                dm=l+1;
+                m--;
+           }
+           for(int j=1;j<dm;j++)
+           {
+               temp=temp->next;
+           }
+          
+            p(temp);
+           
               auto t=temp->next;
               temp->next=NULL;
               temp=t;
