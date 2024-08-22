8a5fb9227f4eda89f0255d0a92897b00e20ee804
Jitendra-rec <65058396+Jitendra-rec@users.noreply.github.com>
2024-08-22 11:28:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (8.34%) - LeetSync

---
+++ 476-number-complement/number-complement.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findComplement(int a) {
+        if(a==0) return 1;
+        int n=log2(a);
+        a=~a;
+        string s=bitset<64>(a).to_string().substr(64-n-1);
+        int ans=stoi(s,0,2);
+        return ans;
+    }
+};
