27563fdc99278d5ce3d4396fa9c35dbe902ad1d4
Jitendra-rec <65058396+Jitendra-rec@users.noreply.github.com>
2024-09-19 17:04:03 +0530

    Time: 6 ms (57.01%) | Memory: 12.1 MB (58.05%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans=0;
+       for(int i=0;i<32;i++)
+       {
+            int cnt=0;
+            for(auto j:nums)
+            {
+                cnt+=(j>>i)&1;
+            }
+            ans=ans | ((cnt%3)<<i);
+       } 
+       return ans;
+    }
+};
