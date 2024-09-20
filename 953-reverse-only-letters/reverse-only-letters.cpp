b70001a8289f0267c62c94770b431850a0d8bf17
Jitendra-rec <65058396+Jitendra-rec@users.noreply.github.com>
2024-09-20 22:28:57 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (62.56%) - LeetSync

---
+++ 953-reverse-only-letters/reverse-only-letters.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+ bool check(char s)
+ {
+     if((s >='a' && s <='z') || (s>='A' && s <='Z'))
+     return true;
+     return false;
+
+ }
+    string reverseOnlyLetters(string s) {
+        
+        int i=0,j=s.length()-1;
+       // cout<<check('_')<<endl;
+        while(i<j)
+        {
+            while(i<j && !check(s[i])) i++;
+            while(j>=0 && !check(s[j])) j--;
+             cout<<i<<" "<<j<<endl;
+             if(i<=j)
+             swap(s[i++],s[j--]);
+        }
+        return s;
+    }
+};
