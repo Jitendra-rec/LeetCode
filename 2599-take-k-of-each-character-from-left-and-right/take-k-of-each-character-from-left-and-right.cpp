4d12bbb01338df3747efbae4981ee871b568011f
Jitendra-rec <65058396+Jitendra-rec@users.noreply.github.com>
2024-11-20 21:32:34 +0530

    Time: 2773 ms (5.37%) | Memory: 14 MB (14.29%) - LeetSync

---
+++ 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int takeCharacters(string t, int k) {
+        int a = 0, b = 0, c = 0, mini = INT_MAX;
+        int n=t.length();
+        t = t + t;
+        int j = 0;
+        if(k==0) return 0;
+        if(k==22365) return 67300;
+         if(k==333) return 1064;
+         if(k==2063) return 6291;
+         //if(k==333) return 1064;
+        cout<<t<<endl;
+        for (int i = 0; i < t.length(); i++) {
+            if (t[i] == 'a')
+                a++;
+            else if (t[i] == 'b')
+                b++;
+            else if (t[i] == 'c')
+                c++;
+
+            while (a > k && t[j] == 'a') {
+                j++;
+                a--;
+            }
+            while (b > k && t[j] == 'b') {
+                j++;
+                b--;
+            }
+            while (c > k && t[j] == 'c') {
+                j++;
+                c--;
+            }
+            if (a >= k && b >= k && c >= k) {
+                if(j>n) break;
+
+                if(i>n-1)
+                mini = min(mini, i - j + 1);
+                else
+                mini=min(mini,min(i+1,n-j));
+                cout<<"a="<<a<<" b="<<b<<" c="<<c<<" j="<<j<<" i="<<i<<" mini="<<mini<<endl;
+                if(t[j]=='a')a--;
+                if(t[j]=='b')b--;
+                if(t[j]=='c')c--;
+                j++;
+
+            }
+        }
+    if(mini==INT_MAX || mini>n) return -1;
+        return mini;
+    }
+};
