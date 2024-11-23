7ac65a3f2159a5e976c0ef762702d5219b5b0ee9
Jitendra-rec <65058396+Jitendra-rec@users.noreply.github.com>
2024-11-23 23:06:48 +0530

    Time: 200 ms (34.17%) | Memory: 56.2 MB (85.37%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.cpp
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
-        int ROWS = box.size();
-        int COLS = box[0].size();
-        
-        vector<vector<char>> res(COLS, vector<char>(ROWS, '.'));
-        
-        for (int r = 0; r < ROWS; r++) {
-            int i = COLS - 1;
-            for (int c = COLS - 1; c >= 0; c--) {
-                if (box[r][c] == '#') {
-                    res[i][ROWS - r - 1] = '#';
-                    i--;
-                }
-                else if (box[r][c] == '*') {
-                    res[c][ROWS - r - 1] = '*';
-                    i = c - 1;
+        int row = box.size();
+        int col = box[0].size();
+        vector<vector<char>> box_90(col, vector<char>(row));
+
+        // Simulate gravity
+        for (int i = 0; i < row; i++) {
+            int cell = col - 1;
+            for (int j = col - 1; j >= 0; j--) {
+                if (box[i][j] == '*') {
+                    cell = j - 1;
+                } else if (box[i][j] == '#') {
+                    box[i][j] = '.';
+                    box[i][cell--] = '#';
                 }
             }
         }
-        
-        return res;
+
+        // Rotate the box 90 degrees clockwise
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                box_90[j][row - i - 1] = box[i][j];
+            }
+        }
+
+        return box_90;
     }
 };
