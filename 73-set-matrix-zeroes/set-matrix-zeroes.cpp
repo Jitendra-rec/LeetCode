b2c7f390469accab9c536601fde221c2b01b5486
Jitendra-rec <65058396+Jitendra-rec@users.noreply.github.com>
2024-12-29 01:18:01 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (13.77%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,29 +1,56 @@
 class Solution {
 public:
-    void solve(vector<vector<int>>& matrix,int a,int b)
-    {
-        int n=matrix.size();
-         int m=matrix[0].size();
-        for(int i=0;i<n;i++)
-        matrix[i][b]=0;
+    // void solve(vector<vector<int>>& matrix,int a,int b)
+    // {
+    //     int n=matrix.size();
+    //      int m=matrix[0].size();
+    //     for(int i=0;i<n;i++)
+    //     matrix[i][b]=0;
 
-         for(int i=0;i<m;i++)
-        matrix[a][i]=0;
-    }
-    void setZeroes(vector<vector<int>>& matrix) {
-        vector<pair<int,int>>pr;
-        int n=matrix.size();
-        int m=matrix[0].size();
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-            if(matrix[i][j]==0)
-                pr.push_back({i,j});
+    //      for(int i=0;i<m;i++)
+    //     matrix[a][i]=0;
+    // }
+    // void setZeroes(vector<vector<int>>& matrix) {
+    //     vector<pair<int,int>>pr;
+    //     int n=matrix.size();
+    //     int m=matrix[0].size();
+    //     for(int i=0;i<n;i++)
+    //         for(int j=0;j<m;j++)
+    //         if(matrix[i][j]==0)
+    //             pr.push_back({i,j});
 
-        for(auto i:pr)
-        {
-           // cout<<i.first<<" = "<<i.second<<endl;
-           solve(matrix,i.first,i.second);
-        }
+    //     for(auto i:pr)
+    //     {
+    //       solve(matrix,i.first,i.second);
+    //     }
         
-    }
+    // }
+
+
+
+     void setZeroes(vector<vector<int>>& mat) {
+         int n=mat.size();
+         int m=mat[0].size();
+        vector<int> row(n,0);
+        vector<int> col(m,0);
+
+
+         for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)
+        {
+            if(mat[i][j]==0)
+            {
+                row[i]=1;
+                col[j]=1;
+            }
+        }
+
+        for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)
+        {
+            if(row[i]==1 || col[j]==1)
+            mat[i][j]=0;
+        }
+     }
+
 };
