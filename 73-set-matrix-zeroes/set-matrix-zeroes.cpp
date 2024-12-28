197075edbd7865dc54b968db4f0ccf98861a792d
Jitendra-rec <65058396+Jitendra-rec@users.noreply.github.com>
2024-12-28 20:31:29 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (27.86%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,43 +1,29 @@
 class Solution {
 public:
-void solve(vector<vector<int>>& matrix,int i,int j){
-    int row=matrix.size();
-    int col=matrix[0].size();
+    void solve(vector<vector<int>>& matrix,int a,int b)
+    {
+        int n=matrix.size();
+         int m=matrix[0].size();
+        for(int i=0;i<n;i++)
+        matrix[i][b]=0;
 
-    for(int k=0;k<col;k++){
-       if( matrix[i][k]!=0)
-           matrix[i][k]=78;
+         for(int i=0;i<m;i++)
+        matrix[a][i]=0;
     }
-     for(int k=0;k<row;k++){
-        if( matrix[k][j]!=0)
-            matrix[k][j]=78;
-    }
-   // cout<<i<<j<<endl;
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<pair<int,int>>pr;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+            if(matrix[i][j]==0)
+                pr.push_back({i,j});
 
-}
-void setZeroes(vector<vector<int>>& matrix) {
-   // vector<vector<int>>ans;
-
-        int row=matrix.size();
-        int col=matrix[0].size();
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(matrix[i][j]==0){
-                    solve(matrix,i,j);
-                }
-            }
+        for(auto i:pr)
+        {
+           // cout<<i.first<<" = "<<i.second<<endl;
+           solve(matrix,i.first,i.second);
         }
-
-                for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(matrix[i][j]==78){
-                     matrix[i][j]=0;
-                  
-                }
-           }
-       }
-
-
         
     }
 };
